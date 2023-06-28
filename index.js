a0fb50052f4bf708d8b1140fa43e14a1ed69f59a
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-06-28 18:09:33 +1200

    wip

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 require("dotenv").config();
 
-console.log(
-  Object.fromEntries(
-    Object.entries(process.env)
-      .filter(([k]) => k.length === 1)
-      .sort((a, b) => {
-        if (a[0] > b[0]) {
-          return 1;
-        } else if (a[0] < b[0]) {
-          return -1;
-        } else {
-          return 0;
-        }
-      })
-  )
-);
+const envVars = Object.entries(process.env);
+
+const filteredEnvVars = envVars
+  .filter(([k]) => k.length === 1)
+  .sort((a, b) => {
+    if (a[0] > b[0]) {
+      return 1;
+    } else if (a[0] < b[0]) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+
+console.log(Object.fromEntries(filteredEnvVars));
